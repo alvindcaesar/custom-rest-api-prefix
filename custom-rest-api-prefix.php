df4d59b9d28ed4d3e507ce9e64703d34eb826df6
Alvind <alvind.caesar@gmail.com>
2022-09-21 16:07:36 +0800

    Initial commit

---
+++ custom-rest-api-prefix.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name:     Custom REST API Prefix
+ * Plugin URI:      https://github.com/alvindcaesar/custom-rest-api-prefix
+ * Description:     A simple plugin to customize the default WordPress REST API prefix (wp-json)
+ * Author:          Alvind Caesar
+ * Author URI:      https://alvindcaesar.com
+ * Text Domain:     custom-rest-api-prefix
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Custom_Rest_Api_Prefix
+ */
+
+if( ! defined('ABSPATH')) die;
+
